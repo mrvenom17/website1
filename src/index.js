bc32f89f2701a6e1f51a9ecaa7a0e26e2f7776e1
Alay Sharma <87763921+mrvenom17@users.noreply.github.com>
2023-08-13 14:56:52 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,153 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Toaster } from "react-hot-toast";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import ThreeGlobe from "three-globe";
+import { WebGLRenderer, Scene } from "three";
+import {
+  PerspectiveCamera,
+  AmbientLight,
+  DirectionalLight,
+  Color,
+  Fog,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import countries from "./files/globe-data.json";
+import geoJsonData from "../custom.geo.json";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-    <Toaster/>
-  </BrowserRouter>
-    
+const coordinates = geoJsonData.features.map((feature) => feature.geometry.coordinates);
 
-);
+
+var renderer, camera, scene, controls;
+let mouseX = 0;
+let mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+var Globe;
+
+init();
+initGlobe();
+onWindowResize();
+animate();
+
+function init() {
+  renderer = new WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  scene = new Scene();
+  scene.add(new AmbientLight(0xbbbbbb, 0.3));
+  scene.background = new Color(0x040d21);
+
+  camera = new PerspectiveCamera();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  var dLight = new DirectionalLight(0xffffff, 0.8);
+  dLight.position.set(-800, 2000, 400);
+  camera.add(dLight);
+
+  var dLight1 = new DirectionalLight(0x7982f6, 1);
+  dLight1.position.set(-200, 500, 200);
+  camera.add(dLight1);
+
+  var dLight2 = new PointLight(0x8566cc, 0.5);
+  dLight2.position.set(-200, 500, 200);
+  camera.add(dLight2);
+
+  camera.position.z = 400;
+  camera.position.x = 0;
+  camera.position.y = 0;
+
+  scene.add(camera);
+
+  scene.fog = new Fog(0x535ef3, 400, 2000);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dynamicDampingFactor = 0.01;
+  controls.enablePan = false;
+  controls.minDistance = 200;
+  controls.maxDistance = 500;
+  controls.rotateSpeed = 0.8;
+  controls.zoomSpeed = 1;
+  controls.autoRotate = false;
+  controls.minPolarAngle = Math.PI / 3.5;
+  controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+  window.addEventListener("resize", onWindowResize, false);
+  document.addEventListener("mousemove", onMouseMove);
+}
+
+function initGlobe() {
+  Globe = new ThreeGlobe({
+    waitForGlobeReady: true,
+    animateIn: true,
+  })
+    .hexPolygonsData(countries.features)
+    .hexPolygonResolution(3)
+    .hexPolygonMargin(0.7)
+    .arcsData(arcs)
+    .arcColor(() => "#9cff00")
+    .arcAltitude(() => 0.05)
+    .arcStroke(() => 0.5)
+    .arcDashLength(0.9)
+    .arcDashGap(4)
+    .arcDashAnimateTime(1000)
+    .arcsTransitionDuration(1000)
+    .arcDashInitialGap((e) => e.order * 1)
+    .showAtmosphere(true)
+    .atmosphereColor("#3a228a")
+    .atmosphereAltitude(0.25)
+    .hexPolygonColor("rgba(255,255,255, 0.7)");
+
+  scene.add(Globe);
+}
+
+function onMouseMove(event) {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  windowHalfX = window.innerWidth / 1.5;
+  windowHalfY = window.innerHeight / 1.5;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  camera.position.x +=
+    Math.abs(mouseX) <= windowHalfX / 2
+      ? (mouseX / 2 - camera.position.x) * 0.005
+      : 0;
+  camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
+  camera.lookAt(scene.position);
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+function getRandomIndices(length) {
+  const index1 = Math.floor(Math.random() * length);
+  let index2 = Math.floor(Math.random() * length);
+  
+  while (index2 === index1) {
+    index2 = Math.floor(Math.random() * length);
+  }
+  
+  return [index1, index2];
+}
+const numberOfArcs = 100; // You can adjust this as needed
+const arcs = [];
+
+for (let i = 0; i < numberOfArcs; i++) {
+  const [index1, index2] = getRandomIndices(coordinates.length);
+  const startCoords = coordinates[index1];
+  const endCoords = coordinates[index2];
+
+  arcs.push({
+    startLat: startCoords[1],
+    startLng: startCoords[0],
+    endLat: endCoords[1],
+    endLng: endCoords[0],
+  });
+}
